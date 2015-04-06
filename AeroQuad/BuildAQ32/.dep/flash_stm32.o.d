7f1a30463fd7cef2727f89aa043cae809d2c117d
Gordon Zhang <gordonz@umich.edu>
2015-04-06 16:23:37 -0400

    First commit of Aeroquad code, set UserConfig.h to correct settings, compiles correctly

---
+++ AeroQuad/BuildAQ32/.dep/flash_stm32.o.d
@@ -0,0 +1,26 @@
+objSTM32/dummy/../AeroQuad32/MapleCompatibility/flash_stm32.o: \
+ /home/gordon/AeroQuad/AeroQuad32/MapleCompatibility/flash_stm32.c \
+ /home/gordon/AeroQuad/AeroQuad32/MapleCompatibility/flash_stm32F2.c \
+ ../Libmaple/libmaple/libmaple/libmaple.h \
+ ../Libmaple/libmaple/libmaple/libmaple_types.h \
+ ../Libmaple/libmaple/libmaple/stm32.h \
+ ../Libmaple/libmaple/libmaple/util.h \
+ ../Libmaple/libmaple/libmaple/delay.h \
+ ../Libmaple/libmaple/libmaple/util.h \
+ /home/gordon/AeroQuad/AeroQuad32/MapleCompatibility/flash_stm32.h
+
+/home/gordon/AeroQuad/AeroQuad32/MapleCompatibility/flash_stm32F2.c:
+
+../Libmaple/libmaple/libmaple/libmaple.h:
+
+../Libmaple/libmaple/libmaple/libmaple_types.h:
+
+../Libmaple/libmaple/libmaple/stm32.h:
+
+../Libmaple/libmaple/libmaple/util.h:
+
+../Libmaple/libmaple/libmaple/delay.h:
+
+../Libmaple/libmaple/libmaple/util.h:
+
+/home/gordon/AeroQuad/AeroQuad32/MapleCompatibility/flash_stm32.h:
