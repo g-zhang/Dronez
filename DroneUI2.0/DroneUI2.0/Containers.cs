77a8508f42049d9517aa790d6d4361a0768975d8
congrieb <congrieb@umich.edu>
2015-11-16 20:37:48 -0500

    added shared variables file

---
+++ DroneUI2.0/DroneUI2.0/Containers.cs
@@ -0,0 +1,17 @@
+﻿public struct Coordinate
+{
+    double x;
+    double y;
+    double z;
+};
+
+public struct SensorData
+{
+    public Coordinate currentGPS;
+    public Coordinate accelermatorData;
+    public Coordinate gyroData;
+    public Coordinate magData;
+    public int batteryLevel;
+};
+
+public enum FlightMode { gpsNav, roadLineDetection, returnHome, landNow};
---
+++ DroneUI2.0/DroneUI2.0/SharedVars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+public class SharedVars
+{
+    public static Image videoFeedImage;
+    public static Image objectDetectionImage;
+    public static Queue<Coordinate> gpsQueue;
+    public static SensorData sensorData;
+    public static FlightMode flightMode;
+    public static char[] sendBuffer;
+    public static char[] receiveBuffer;
+
+
+
+
+    public SharedVars()
+    {
+    }
+}
